e46068185e371968a5a6dacdbd39d7994a792b9c
Alex Ivanov <oleksii120@gmail.com>
2024-01-31 19:22:11 +0200

    Added checkout controller

--- sephora-backend/Application/Services/Interfaces/ICheckoutService.cs
+++ sephora-backend/Application/Services/Interfaces/ICheckoutService.cs
@@ -5,5 +5,5 @@ namespace CleanArchitecture.Application.Services.Interfaces;
 public interface ICheckoutService
 {
     Task CheckoutUnauthed(IEnumerable<CreateCartDto> cartItems, CreateDeliveryDto delivery);
-    Task CheckoutAuthed(IEnumerable<CartItem> cartItems, ClaimsPrincipal user);
+    Task CheckoutAuthed(ClaimsPrincipal user);
 }

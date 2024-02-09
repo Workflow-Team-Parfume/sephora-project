454e3865c59841fd98d1ad531ac941aac9274f01
Alex Ivanov <oleksii120@gmail.com>
2024-02-09 15:53:05 +0200

    Overall patches to product-related DTOs

--- sephora-backend/Application/Dtos/Product/ProductDTO.cs
+++ sephora-backend/Application/Dtos/Product/ProductDTO.cs
@@ -5,8 +5,8 @@ public class ProductDto
     public long Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string? Description { get; set; }
-    public string ImgPath { get; set; } = String.Empty;
     public bool Active { get; set; }
     public BrandDto Brand { get; set; }
     public CategoryDto Category { get; set; }
+    public IEnumerable<ProductPieceDTO> ProductPieces { get; set; }
 }
--- sephora-backend/Application/Dtos/ProductPiece/CreateProductPieceDto.cs
+++ sephora-backend/Application/Dtos/ProductPiece/CreateProductPieceDto.cs
@@ -3,8 +3,8 @@
 public class CreateProductPieceDto
 {
     public int? InStock { get; set; }
-
     public decimal Price { get; set; }
-
     public int AmountId { get; set; }
+    public bool IsBottledParfume { get; set; }
+    public long ProductId { get; set; }
 }

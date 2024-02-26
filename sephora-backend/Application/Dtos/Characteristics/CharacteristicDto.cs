5ef1644a215400c7f5f8904de4f7d7a3acbc3c1c
Alex Ivanov <oleksii120@gmail.com>
2024-02-26 23:46:11 +0200

    Added CharacteristicsDto

--- sephora-backend/Application/Dtos/Characteristics/CharacteristicDto.cs
+++ sephora-backend/Application/Dtos/Characteristics/CharacteristicDto.cs
@@ -1,6 +1,11 @@
 namespace CleanArchitecture.Application.Dtos.Characteristics;
 
-public class CharacteristicDto
-{
-    
-}
+public record CharacteristicDto(
+    // if Id is 0, it means that the characteristic is new
+    // else just update the existing one
+    long Id = 0,
+    string NameEn = "",
+    string NameUa = "",
+    string ValueEn = "",
+    string ValueUa = ""
+);
--- sephora-backend/Application/Dtos/Rating/RatingDto.cs
+++ sephora-backend/Application/Dtos/Rating/RatingDto.cs
@@ -3,6 +3,7 @@ namespace CleanArchitecture.Application.Dtos.Rating;
 public class RatingDto
 {
     public long Id { get; set; }
+    // TODO: add user info mapping
     // public string? UserName { get; set; }
     // public string? UserPfp { get; set; }
     public ProductDto Product { get; set; } = default!;

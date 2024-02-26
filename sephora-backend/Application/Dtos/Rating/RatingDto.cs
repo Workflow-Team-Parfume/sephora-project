a71370f0d754ad7cf6c27f6420b5e1d5ad9e3344
Alex Ivanov <oleksii120@gmail.com>
2024-02-26 21:25:40 +0200

    Fixed exposure of DB models

--- sephora-backend/Application/Dtos/Rating/RatingDto.cs
+++ sephora-backend/Application/Dtos/Rating/RatingDto.cs
@@ -5,7 +5,7 @@ public class RatingDto
     public long Id { get; set; }
     // public string? UserName { get; set; }
     // public string? UserPfp { get; set; }
-    public ProductEntity? Product { get; set; }
+    public ProductDto Product { get; set; } = default!;
     public decimal Rate { get; set; }
     public string? Comment { get; set; }
 }

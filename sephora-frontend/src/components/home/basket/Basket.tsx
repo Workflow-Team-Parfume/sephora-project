1db40972940f7af61d8ed35f35428145635bd39a
Софія Степанюк <sopha86@gmail.com>
2024-03-02 17:22:19 +0200

    Fixed

--- sephora-frontend/src/components/home/basket/Basket.tsx
+++ sephora-frontend/src/components/home/basket/Basket.tsx
@@ -9,7 +9,7 @@ import icon2 from "../../../assets/images/icon2.svg";
 import { CalculateProductTotal } from "../../../common/calculateTotal";
 
 const style = {
-    position: 'absolute' as 'absolute',
+    position: 'absolute' as const,
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',

6f365098ab0a10a9034b28024c5a650c6a2af343
Alex Ivanov <oleksii120@gmail.com>
2024-03-04 16:15:05 +0200

    Removed unnecessary comments

--- sephora-frontend/src/components/home/productsPage/core/SortingOrder.ts
+++ sephora-frontend/src/components/home/productsPage/core/SortingOrder.ts
@@ -1,40 +1,28 @@
 export default interface SortingOrder {
-    // get nameEn(): string;
-    // get nameUa(): string;
     get key(): string;
     get value(): string;
 }
 
 export const Orders: SortingOrder[] = [
     {
-        // nameEn: 'price',
-        // nameUa: 'вартістю',
         key: 'sortBy.price',
         value: 'price'
     },
     {
         key: 'sortBy.popularity',
-        // nameEn: 'popularity',
-        // nameUa: 'популярністю',
         value: 'product.AverageRating'
     },
     {
         key: 'sortBy.date',
-        // nameEn: 'date',
-        // nameUa: 'датою',
         value: 'createdAt'
     },
 ]
 export const Directions: SortingOrder[] = [
     {
-        // nameEn: 'to low',
-        // nameUa: 'за спаданням',
         key: 'sortBy.toLow',
         value: 'desc'
     },
     {
-        // nameEn: 'to high',
-        // nameUa: 'за зростанням',
         key: 'sortBy.toHigh',
         value: 'asc'
     },

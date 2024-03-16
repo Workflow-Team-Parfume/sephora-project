f0587368dbd208dfe1faae996f66f41f32c9ec43
Soneta <sopha.sts86@gmail.com>
2024-03-16 11:47:48 +0200

    Some changes

--- sephora-frontend/src/components/home/products/Products.tsx
+++ sephora-frontend/src/components/home/products/Products.tsx
@@ -38,7 +38,7 @@ const Products: React.FC<{
                 <Grid container spacing={2}>
                     {products.length > 0 ?
                         products.map((product) => (
-                            <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                            <Grid key={product.id} item xs={6} sm={6} md={4} lg={3}>
                                 <Product key={product.id} piece={product}/>
                             </Grid>
                         ))

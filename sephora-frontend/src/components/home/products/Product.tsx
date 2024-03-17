166afb9f33bd01af04d27ce4e93c3758f5194913
Soneta666 <90927771+Soneta666@users.noreply.github.com>
2024-03-17 14:27:57 +0200

    Add filters and some changes (#66)
    * Some changes
    
    * Some changes
    
    * Add filters

--- sephora-frontend/src/components/home/products/Product.tsx
+++ sephora-frontend/src/components/home/products/Product.tsx
@@ -1,15 +1,17 @@
-import {Button, Card, CardMedia, Link, Rating, Stack, Typography} from "@mui/material";
+import {Button, Card, CardMedia, FormControl, Link, MenuItem, Rating, Select, Stack, Typography} from "@mui/material";
 import "./products.scss"
 import StarIcon from "@mui/icons-material/Star";
 import ProductPieceDto from "../../../models/piece/ProductPieceDto.ts";
 import React from "react";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
 import {useTranslation} from "react-i18next";
 import routes from "../../../common/routes.ts";
 import i18n from "i18next";
 import changeFavStatus from "./ChangeFavStatus.ts";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../store/store.ts";
+import textFieldStyle from '../../../common/textFieldStyle';
 
 function IsNew(isNew: boolean) {
     if (isNew) {
@@ -25,24 +27,29 @@ const Product: React.FC<{ piece: ProductPieceDto }>
     = ({piece}) => {
     const {t} = useTranslation();
     const isAuthed = useSelector((store: RootState) => store.auth.isAuth);
+    const [isHovered, setIsHovered] = React.useState(false);
 
     const handleFavClick = () => {
         // TODO: change styling
         changeFavStatus(piece.product.id, isAuthed);
     };
 
+    
+
     // TODO: Change link
     return (
-        <Link href={`/details/${piece.product.id}?piece=${piece.id}`} underline="none">
+        <Card className="productMainContainer"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}>
+            {IsNew(piece.isNew)}
+            <Button className="favorite" onClick={handleFavClick} disableTouchRipple>
+                <FavoriteBorderIcon sx={{width:"30px", height:"30px"}}/>
+            {/* <FavoriteIcon sx={{width:"30px", height:"30px"}}/> */}
+            </Button>
+            <Link href={`/details/${piece.product.id}?piece=${piece.id}`} underline="none" >
 
-            <Card className="productMainContainer"
-                  sx={{height: '95%'}}>
-                {IsNew(piece.isNew)}
-                <Button className="favorite" onClick={handleFavClick}>
-                    <FavoriteBorderIcon className="favorite"/>
-                </Button>
-                {/* <FavoriteIcon className="favorite"/> */}
-                <Stack spacing={2} direction='column'>
+                <Stack spacing={2} direction='column'
+                    sx={{ padding: "12px 10px" }} >
 
                     <CardMedia
                         component="div"
@@ -86,8 +93,46 @@ const Product: React.FC<{ piece: ProductPieceDto }>
                         </Stack>
                     </Stack>
                 </Stack>
-            </Card>
-        </Link>
+            </Link>
+            {isHovered && (
+                <Stack
+                    sx={{
+                        display: isHovered ? 'block' : 'none',
+                        position: 'absolute',
+                        bottom: '-25%',
+                        zIndex: 1,
+                        width: '100%',
+                        padding: "-10px",
+                    }}>
+                    <Stack 
+                        className="hoverProd"
+                        spacing={1}
+                    >
+                        {piece.product.volumes.length != 0 ?
+                            <FormControl fullWidth sx={{...textFieldStyle}}>
+                                <Select
+                                    sx={{width: '100%',
+                                    borderRadius: 0}}
+                                    value={piece.id}
+                                    // onChange={(e) => changePiece(Number(e.target.value), product)}
+                                    displayEmpty>
+                                    {piece.product.volumes?.map((volume, index) => (
+                                        <MenuItem key={index} /*value={piece.product.pieces[index].id}*/
+                                        >
+                                            <Typography className="productVolume">
+                                                {volume.milliliters} {t('common.ml')}
+                                            </Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            : <></>
+                        }
+                            <Button fullWidth className="butBuy">{t('details.buy')}</Button>
+                    </Stack>
+                </Stack>
+                )}
+        </Card>
     );
 }
 
